420db2b3ec71f92bb7afecbabfe5e71b2ccf0122
Luis Rondon <leduardo.rondon@gmail.com>
2023-01-06 22:22:35 -0400

    Add error boundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,14 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import { getUsersService } from './services/userService';
+import React, { Fragment } from 'react';
+import ErrorBoundary from './components/ErrorBoundary';
+import UsersPage from './pages/UsersPage';
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [filter, setFilter] = useState("");
-
-  const getUsersPlaceholder = async () => {
-    const usersData = await getUsersService();
-    setUsers(usersData);
-  }
-
-  const showDetail = (name, address) => {
-    alert(`nombre ${name} Dirección ${address.street}`);
-  }
-
-  const filterUsers = useMemo(() => {
-    return users.filter((user) => user.name.toUpperCase().includes(filter.toUpperCase()));
-  }, [users, filter]);
-
-  useEffect(() => {
-    getUsersPlaceholder();
-    return () => {
-      setUsers([]);
-    }
-  }, []);
-
   return (
-    <div>
-      <input type="text" value={filter} onChange={(ev) => setFilter(ev.target.value) } />
-      <ul>
-        {Object.keys(users).map((key, user) => {
-          const { name, email, address } = users[user];
-          return <div key={key}>
-              <li>{name} - {email}</li>
-              <button onClick={() => showDetail(name,address)}>Details</button>
-          </div>
-        })}
-      </ul>
-      <ul>
-        {filterUsers.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-  </div>
+    <Fragment>
+      <ErrorBoundary>
+        <UsersPage />
+      </ErrorBoundary>
+    </Fragment>
   );
 }
 
---
+++ src/components/ErrorBoundary.jsx
@@ -0,0 +1,31 @@
+import { Component } from "react";
+
+export default class ErrorBoundary extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(_) {
+      // Update state so the next render will show the fallback UI.
+      return { hasError: true };
+    }
+
+    componentDidCatch(error, errorInfo) {
+      // You can also log the error to an error reporting service
+      console.error("Uncought error", error, errorInfo);
+    }
+
+    resetError(){
+        this.setState({ hasError: false})
+    }
+
+    render() {
+      if (this.state.hasError) {
+        // You can render any custom fallback UI
+        return <h1>Something went wrong.</h1>;
+      }
+
+      return this.props.children;
+    }
+  }
